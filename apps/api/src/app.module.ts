3f7076c02548bc055dc741ffadc902754f84b40a
HaroldHLM <haroldlaymedev@gmail.com>
2026-06-04 10:37:47 -0500

    Modificando app.module para que contenga el modulo de Period

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,6 +6,7 @@ import { AttendancesModule } from './modules/attendances/attendances.module';
 import { CoursesModule } from './modules/courses/courses.module';
 import { EnrollmentsModule } from './modules/enrollments/enrollments.module';
 import { InstitutionsModule } from './modules/institutions/institutions.module';
+import { PeriodsModule } from './modules/periods/periods.module';
 import { StudentsModule } from './modules/students/students.module';
 import { SubjectsModule } from './modules/subjects/subjects.module';
 import { TeachersModule } from './modules/teachers/teachers.module';
@@ -22,6 +23,7 @@ import { TeachersModule } from './modules/teachers/teachers.module';
     InstitutionsModule,
     TeachersModule,
     SubjectsModule,
+    PeriodsModule,
   ],
 })
 export class AppModule {}
